5655851575d8970e4fb0b9636f64bbe6e8e312da
dharmveer bangar <dvbangar8@gmail.com>
2020-01-07 11:17:44 +0530

    add new images

--- src/components/TestimonialsData.js
+++ src/components/TestimonialsData.js
@@ -3,19 +3,28 @@ import Testimonials from './Testimonials';
 
 const TestimonialsData = () => (
   <div className="columns is-multiline">
-    <Testimonials bgImage="/images/gallery/dummy.png" title="Beautiful Bride" />
     <Testimonials
-      bgImage="/images/gallery/dummy.png"
+      bgImage="/images/gallery/photo-1@2x.png"
+      title="Beautiful Bride"
+    />
+    <Testimonials
+      bgImage="/images/gallery/photo-2@2x.png"
       title="Weddings at deckerdence"
     />
-    <Testimonials bgImage="/images/gallery/dummy.png" title="Sweet Treats" />
     <Testimonials
-      bgImage="/images/gallery/dummy.png"
+      bgImage="/images/gallery/photo-3@2x.png"
+      title="Sweet Treats"
+    />
+    <Testimonials
+      bgImage="/images/gallery/photo-4@2x.png"
       title="celebrate with loved ones"
     />
-    <Testimonials bgImage="/images/gallery/dummy.png" title="Summer Bouquet" />
     <Testimonials
-      bgImage="/images/gallery/dummy.png"
+      bgImage="/images/gallery/photo-5@2x.png"
+      title="Summer Bouquet"
+    />
+    <Testimonials
+      bgImage="/images/gallery/photo-6@2x.png"
       title="weddings that sparkle"
     />
   </div>
