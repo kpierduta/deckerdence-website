0652094367ddb4f3e2e0fff786514361e521db33
dharmveer97 <dvbangar8@gmail.com>
2020-01-10 13:10:17 +0530

    minior changes

--- src/components/HireOptions.js
+++ src/components/HireOptions.js
@@ -6,26 +6,34 @@ const Section = styled.div`
   background-image: url(${props => props.hirebgImage});
   background-size: contain;
   background-repeat: no-repeat;
+  @media screen and (max-width: 768px) {
+    background-size: cover;
+  }
   .text {
     font-size: 6rem;
+    padding: 0rem 9rem;
+    @media screen and (max-width: 768px) {
+      padding: 0rem 0rem;
+    }
   }
 `;
 
 class HireOptions extends React.Component {
   render() {
-    const { hirebgImage } = this.props;
+    const { hirebgImage, mainTitle, subTitle, centerText } = this.props;
     return (
       <Section className="section hero is-large" hirebgImage={hirebgImage}>
         <div className="container">
           <div className="columns is-vcentered is-desktop">
-            <div className="column has-text-centered-desktop">
-              <h2 className="title is-1 has-text-white">Option 1</h2>
-              <h2 className="text subtitle is-1 has-text-weight-bold has-text-white">
-                Deckerdence <br />
-                Vintage Bus Bar
+            <div className="column has-text-centered ">
+              <h2 className="title is-spaced is-1 has-text-white is-size-6-mobile">
+                {mainTitle}
               </h2>
-              <h2 className="subtitle is-1 has-text-white">
-                BESPOKE DRINKS OPTIONS AVAILABLE
+              <h2 className="text subtitle is-1 has-text-weight-bold has-text-white is-size-5-mobile">
+                {centerText}
+              </h2>
+              <h2 className="subtitle is-1 has-text-white is-size-6-mobile">
+                {subTitle}
               </h2>
             </div>
           </div>
--- src/pages/hirePage.js
+++ src/pages/hirePage.js
@@ -11,15 +11,21 @@ export default class HirePage extends React.Component {
     return (
       <Layout>
         <Seo title="Home" description="Welcome to GatsbyJs v1" />
-        <HireOptions hirebgImage="/images/hire/header-photo@2x.png" />
+        <HireOptions
+          hirebgImage="/images/hire/header-photo@2x.png"
+          mainTitle="Option 1"
+          centerText="Deckerdence Vintage Bus Bar"
+          subTitle="BESPOKE DRINKS OPTIONS AVAILABLE"
+        />
         <OptionToIncludes />
         <HireForm
-          para=""
+          para="Deckerdence Bus Bar with the 1/2 wrap-around marquee is a great option for entertaining. Perfect for pitchside hospitality, or a racecourse or polo match. Our team will come along and carry out a site visit to your location, and then we will work with you to ensure you have everything perfect for your event. We have a range of items that furnish Deckerdence perfectly."
           avatar="/images/hire/icon@2x.png"
           email="Email"
           phone="Phone Number"
           buttonName="DOWNLOAD"
           subtitle="By submitting this form you agree to our contact conditions."
+          mainSubtitle="mainSubtitle"
         />
       </Layout>
     );
