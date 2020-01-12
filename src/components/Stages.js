685e31b538ae06384b60a1f99288e5934d17fc1a
dharmveer97 <dvbangar8@gmail.com>
2020-01-13 00:28:37 +0530

    fix image position

--- src/components/Stages.js
+++ src/components/Stages.js
@@ -1,19 +1,26 @@
 import React from 'react';
 
+import styled from 'styled-components';
+
+const ImageWrapper = styled.div`
+  justify-content: center;
+`;
 class Stages extends React.Component {
   render() {
     const { title, subtitle } = this.props;
     return (
       <section className="column is-4 is-multiline">
-        <div className="card">
+        <div className=" card-content">
           <div className="card-image">
-            <figure className="image is-128x128">
-              <img
-                className="is-rounded"
-                src="/images/parties/icon@2x.png"
-                alt=""
-              />
-            </figure>
+            <ImageWrapper className="media align">
+              <figure className="image is-128x128">
+                <img
+                  className="is-rounded"
+                  src="/images/parties/icon@2x.png"
+                  alt=""
+                />
+              </figure>
+            </ImageWrapper>
           </div>
           <div className="card-content">
             <p className="title is-3 has-text-weight-normal is-spaced has-text-centered">
