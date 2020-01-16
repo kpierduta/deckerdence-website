462abe4c325c252c4beb6b5f7aa0eae9b5a671a8
dharmveer97 <dvbangar8@gmail.com>
2020-01-16 14:59:05 +0530

    add google map

---
+++ src/components/Address.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import MyMapComponent from './GoogleMap';
+
+const Section = styled.div`
+  background-color: ${props => props.theme.textColorLite};
+  .columns {
+    background-image: url('/images/contacts/frame@2x.png');
+    background-size: contain;
+    background-repeat: no-repeat;
+  }
+  .is-10 {
+    padding: 6.5rem 0rem;
+  }
+`;
+
+const Address = () => (
+  <Section className="section">
+    <div className="container">
+      <div className="columns is-centered">
+        <div className="column is-10">
+          <MyMapComponent isMarkerShown className="is-hidden-mobile" />
+        </div>
+      </div>
+    </div>
+  </Section>
+);
+
+export default Address;
--- src/pages/contactPage.js
+++ src/pages/contactPage.js
@@ -3,8 +3,8 @@ import React from 'react';
 import Seo from '../components/Seo';
 import Layout from '../components/Layout';
 import PageHeading from '../components/PageHeading';
-
 import ContactUsData from '../components/ContactUsData';
+import Address from '../components/Address';
 
 export default class ContactPage extends React.Component {
   render() {
@@ -13,6 +13,7 @@ export default class ContactPage extends React.Component {
         <Seo title="Home" description="Welcome to GatsbyJs v1" />
         <PageHeading title="Contact Us" />
         <ContactUsData />
+        <Address />
       </Layout>
     );
   }
