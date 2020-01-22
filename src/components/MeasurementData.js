3dc6d35869938f513324cda121be6bb939693e88
dharmveer97 <dvbangar8@gmail.com>
2020-01-22 11:54:29 +0530

    clean up

--- src/components/MeasurementData.js
+++ src/components/MeasurementData.js
@@ -1,33 +1,42 @@
 import React from 'react';
 
-import Measurement from './Measurement';
+import BarTile from './BarTile';
 
 class MeasurementData extends React.Component {
   render() {
     return (
       <section className="section">
         <div className="container">
-          <div className="columns is-multiline">
-            <Measurement
-              title="Marquee information"
-              subtitle=" The half wrap around marquee measures 25m x 9m and the marquee structure is fitted directly from the bus, between the  decks, to not spoil the upstairs views."
-            />
-            <Measurement
-              title="Marquee information"
-              subtitle=" The half wrap around marquee measures 25m x 9m and the marquee structure is fitted directly from the bus, between the  decks, to not spoil the upstairs views."
-            />
-            <Measurement
-              title="Marquee information"
-              subtitle=" The half wrap around marquee measures 25m x 9m and the marquee structure is fitted directly from the bus, between the  decks, to not spoil the upstairs views."
-            />
-            <Measurement
-              title="Marquee information"
-              subtitle=" The half wrap around marquee measures 25m x 9m and the marquee structure is fitted directly from the bus, between the  decks, to not spoil the upstairs views."
-            />
+          <div className="tile is-ancestor">
+            <div className="tile is-6 is-vertical is-parent">
+              <BarTile
+                title="Marquee information"
+                subtitle="The half wrap around marquee measures 25m x 9m and the marquee structure is fitted directly from the bus, between the
+                2 decks, to not spoil the upstairs views."
+              />
+              <BarTile
+                title="Marquee information"
+                subtitle="The half wrap around marquee measures 25m x 9m and the marquee structure is fitted directly from the bus, between the
+                2 decks, to not spoil the upstairs views."
+              />
+            </div>
+            <div className="tile is-6 is-vertical is-parent">
+              <BarTile
+                title="Marquee information"
+                subtitle="The half wrap around marquee measures 25m x 9m and the marquee structure is fitted directly from the bus, between the
+                2 decks, to not spoil the upstairs views."
+              />
+              <BarTile
+                title="Marquee information"
+                subtitle="The half wrap around marquee measures 25m x 9m and the marquee structure is fitted directly from the bus, between the
+                2 decks, to not spoil the upstairs views."
+              />
+            </div>
           </div>
         </div>
       </section>
     );
   }
 }
+
 export default MeasurementData;
