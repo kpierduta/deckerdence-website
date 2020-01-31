c695f7554cae7fdad555fcbacb33bd0554f539c2
dharmveer97 <dvbangar8@gmail.com>
2020-01-31 15:45:09 +0530

    add new components

---
+++ src/components/Gallery.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import styled from 'styled-components';
+
+const Section = styled.div``;
+
+class Gallery extends React.Component {
+  render() {
+    const {} = this.props;
+    return (
+      <Section className="section">
+        <div className="columns is-varialbe is-5 is-multiline">
+          <div className="column is-4">
+            <figure className="image is-3by2">
+              <img src="/images/gallery/goin.png" alt="gallery" />
+            </figure>
+          </div>
+          <div className="column is-4">
+            <figure className="image is-3by2">
+              <img src="/images/gallery/goin.png" alt="gallery" />
+            </figure>
+          </div>
+          <div className="column is-4">
+            <figure className="image is-3by2">
+              <img src="/images/gallery/goin.png" alt="gallery" />
+            </figure>
+          </div>
+          <div className="column is-4">
+            <figure className="image is-3by2">
+              <img src="/images/gallery/goin.png" alt="gallery" />
+            </figure>
+          </div>
+          <div className="column is-4">
+            <figure className="image is-3by2">
+              <img src="/images/gallery/goin.png" alt="gallery" />
+            </figure>
+          </div>
+          <div className="column is-4">
+            <figure className="image is-3by2">
+              <img src="/images/gallery/goin.png" alt="gallery" />
+            </figure>
+          </div>
+        </div>
+      </Section>
+    );
+  }
+}
+
+export default Gallery;
