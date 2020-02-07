f10b9c63e346d88f9bf21c31583287aff44f2390
gagansaini1212 <gagansaini1212@gmail.com>
2020-02-07 18:46:37 +0530

    fix eslint errors

--- src/components/HomePageSection.js
+++ src/components/HomePageSection.js
@@ -6,7 +6,7 @@ const Section = styled.section`
   background-image: url(${props => props.bgImg});
   background-size: cover;
   .columns {
-    justify-content: ${props => props.flex ? 'flex-end' : 'flex-start'};
+    justify-content: ${props => (props.flex ? 'flex-end' : 'flex-start')};
   }
   .bg-image {
     height: 46rem;
@@ -19,7 +19,6 @@ const Section = styled.section`
     display: grid;
     align-content: center;
     padding: 1rem 4rem;
-
   }
   .subtitle.is-5 {
     font-family: ${props => props.theme.primaryFontFamily};
@@ -29,21 +28,21 @@ const Section = styled.section`
 
 const HomePageSection = ({ title, description, flex, bgImg, bannerImg }) => (
   <Section flex={flex} bgImg={bgImg} bannerImg={bannerImg}>
-  <div className="container">
-    <div className="columns">
-      <div className="column bg-image is-6">
-            <h1 className="title is-2 has-text-white has-text-weight-semibold	is-spaced">
-              {title}
-            </h1>
-            <h1 className="subtitle is-5 has-text-white has-text-weight-normal">
-              {description}
-            </h1>
-            <Button
-              image="/images/learn-more@2x.png"
-              alt="Learn More Button"
-              hasWidth="50%"
-            />
-          </div>
+    <div className="container">
+      <div className="columns">
+        <div className="column bg-image is-6">
+          <h1 className="title is-2 has-text-white has-text-weight-semibold	is-spaced">
+            {title}
+          </h1>
+          <h1 className="subtitle is-5 has-text-white has-text-weight-normal">
+            {description}
+          </h1>
+          <Button
+            image="/images/learn-more@2x.png"
+            alt="Learn More Button"
+            hasWidth="50%"
+          />
+        </div>
       </div>
     </div>
   </Section>
