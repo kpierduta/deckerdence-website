9c3a6f8197252893de531814cd9cd9beb478e371
gagansaini1212 <gagansaini1212@gmail.com>
2020-02-07 18:29:38 +0530

    clean up

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,33 +9,47 @@ const Index = () => (
   <Layout>
     <Seo title="Home" description="Welcome to GatsbyJs v1" />
     <HomeHero />
-    <HomePageSection title="WEDDINGS" description="Say yes to the perfect wedding venue.
+    <HomePageSection
+      title="WEDDINGS"
+      description="Say yes to the perfect wedding venue.
       Have your special day at our event space in the Warwickshire countryside or take our
       vintage mobile venue to a location of your choosing. Whether you
       want to go traditional with silver service or festival-style
       with the best street food vendors, we can create your dream
       wedding."
       bgImg="/images/events/photo@3x.png"
-      bannerImg="/images/events/box-1.png" />
-    <HomePageSection flex title="PARTIES" description="Throw the best party in town.
+      bannerImg="/images/events/box-1.png"
+    />
+    <HomePageSection
+      flex
+      title="PARTIES"
+      description="Throw the best party in town.
       No matter what the occasion, our vintage bus provides an incredible
       focal point for your party. Serving up traditional drinks and cocktails,
       with an old English drawing room inspired lounge on-board, we can deliver
       the vintage party for you."
       bgImg="/images/events/party@3x.png"
-      bannerImg="/images/events/box-2.png" />
-    <HomePageSection title="EVENTS" description="Make your event stand out from the crowd.
+      bannerImg="/images/events/box-2.png"
+    />
+    <HomePageSection
+      title="EVENTS"
+      description="Make your event stand out from the crowd.
       With a classical bar and luxurious cocktail lounge, our vintage bus provides a stunning
       feature for your event. Perfect for festivals, fêtes, fairs and exhibitions, our unique
       mobile venue can travel anywhere in the UK."
       bgImg="/images/events/event@3x.png"
-      bannerImg="/images/events/box-3.png" />
-    <HomePageSection flex title="Hospitality" description="Deliver an event with a difference.
+      bannerImg="/images/events/box-3.png"
+    />
+    <HomePageSection
+      flex
+      title="Hospitality"
+      description="Deliver an event with a difference.
       Whether you’re hosting a corporate event, fashion show, award ceremony or ball, Deckerdence
       provides a unique space to entertain. Complete with bar, cocktail lounge and a bird’s eye
       view at sporting events, our vintage bus will give your clients a day to remember."
       bgImg="/images/events/Hospitality@3x.png"
-      bannerImg="/images/events/box-4.png" />
+      bannerImg="/images/events/box-4.png"
+    />
   </Layout>
 );
 
