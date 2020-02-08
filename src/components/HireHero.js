84e419f9e1489d3de1bbb6cb07a968e80fafdab0
gagansaini1212 <gagansaini1212@gmail.com>
2020-02-08 14:02:44 +0530

    hire hero chamged to stateless

--- src/components/HireHero.js
+++ src/components/HireHero.js
@@ -15,31 +15,25 @@ const Section = styled.section`
     font-size: 4rem;
   }
 `;
-
-class HireHero extends React.Component {
-  render() {
-    const { title, subtitle, para } = this.props;
-    return (
-      <Section className="hero is-primary is-medium">
-        <div className="hero-body">
-          <div className="container">
-            <div className="columns is-centered">
-              <div className="column is-9 has-text-centered">
-                <h1 className="title is-2 has-text-white has-text-weight-normal">
-                  {title}
-                </h1>
-                <h1 className="title is-1 has-text-white has-text-weight-semibold	">
-                  {subtitle}
-                </h1>
-                <img src="/images/hire/decorative-line.png" alt="" />
-                <h1 className="subtitle is-3 has-text-white">{para}</h1>
-              </div>
-            </div>
+const HireHero = ({ title, subtitle, para }) => (
+  <Section className="hero is-primary is-medium">
+    <div className="hero-body">
+      <div className="container">
+        <div className="columns is-centered">
+          <div className="column is-9 has-text-centered">
+            <h1 className="title is-2 has-text-white has-text-weight-normal">
+              {title}
+            </h1>
+            <h1 className="title is-1 has-text-white has-text-weight-semibold	">
+              {subtitle}
+            </h1>
+            <img src="/images/hire/decorative-line.png" alt="" />
+            <h1 className="subtitle is-3 has-text-white">{para}</h1>
           </div>
         </div>
-      </Section>
-    );
-  }
-}
+      </div>
+    </div>
+  </Section>
+);
 
 export default HireHero;
