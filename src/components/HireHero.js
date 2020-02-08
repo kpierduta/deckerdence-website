26a74947b7c4b91945d1bfbf9a1b3e703e0346d2
gagansaini1212 <gagansaini1212@gmail.com>
2020-02-08 13:44:39 +0530

    hire hero web view fixed

--- src/components/HireHero.js
+++ src/components/HireHero.js
@@ -6,10 +6,13 @@ const Section = styled.section`
   .column {
     background-image: url('/images/hire/frame.png');
     background-repeat: no-repeat;
-    height: 34rem;
+    background-position: cover;
+    background-size: 100% 100%;
+    padding: 5rem;
+    justify-content: space-between;
   }
-  .title.is-4 {
-    margin-top: 2rem;
+  .title.is-1 {
+    font-size: 4rem;
   }
 `;
 
@@ -17,22 +20,20 @@ class HireHero extends React.Component {
   render() {
     const { title, subtitle, para } = this.props;
     return (
-      <Section className="section">
-        <div className="container">
-          <div className="columns  is-centered">
-            <div className="column is-9 has-text-centered">
-              <section className="section">
-                <div className="container">
-                  <h1 className="title is-4 has-text-white has-text-weight-normal">
-                    {title}
-                  </h1>
-                  <h1 className="title is-1 has-text-white has-text-weight-semibold	">
-                    {subtitle}
-                  </h1>
-                  <img src="/images/hire/decorative-line.png" alt="" />
-                  <h1 className="subtitle is-4 has-text-white">{para}</h1>
-                </div>
-              </section>
+      <Section className="hero is-primary is-medium">
+        <div className="hero-body">
+          <div className="container">
+            <div className="columns is-centered">
+              <div className="column is-9 has-text-centered">
+                <h1 className="title is-2 has-text-white has-text-weight-normal">
+                  {title}
+                </h1>
+                <h1 className="title is-1 has-text-white has-text-weight-semibold	">
+                  {subtitle}
+                </h1>
+                <img src="/images/hire/decorative-line.png" alt="" />
+                <h1 className="subtitle is-3 has-text-white">{para}</h1>
+              </div>
             </div>
           </div>
         </div>
