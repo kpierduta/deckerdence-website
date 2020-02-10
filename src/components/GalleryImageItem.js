0017c49985cc941054df1f2bc88ddce5c6af46fd
gagansaini1212 <gagansaini1212@gmail.com>
2020-02-10 18:22:39 +0530

    added gallery image item

--- src/components/GalleryImageItem.js
+++ src/components/GalleryImageItem.js
@@ -2,14 +2,20 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
+  position: relative;
   .overlay {
     overflow: hidden;
     background-color: #000;
-    height: 600px;
+    height: 100%;
   }
   img {
     object-fit: cover;
     opacity: 0.4;
+  }
+  .content {
+    position: absolute;
+    top: 15rem;
+    width: 100%;
   }
 `;
 
@@ -17,7 +23,10 @@ const GalleryImageItem = () => (
   <Container className="column is-4">
     <div className="overlay">
       <img src="/images/gallery/photo-2@2x.png" alt="" />
-      <div className="has-text-black">The Perfect VenuE</div>
+      <div className="content has-text-centered">
+        <div className="has-text-white">THE PERFECT VENUE</div>
+        <img src="/images/gallery/decorative-line@2x.png" alt="" />
+      </div>
     </div>
   </Container>
 );
