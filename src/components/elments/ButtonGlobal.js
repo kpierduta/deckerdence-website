eea099bc155e384a61148d4639bbe13f5b5f631a
gagansaini1212 <gagansaini1212@gmail.com>
2020-02-13 16:01:52 +0530

    completed tow bar page

---
+++ src/components/elments/ButtonGlobal.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background: transparent;
+  border: 2px solid #fff;
+  :hover,
+  :focus {
+    border: 2px solid #fff;
+  }
+`;
+
+const ButtonGlobal = ({ title }) => (
+  <Button className="button is-outlined is-large is-radiusless" type="button">
+    <span className="is-uppercase has-text-weight-bold has-text-white">
+      {title}
+    </span>
+  </Button>
+);
+
+export default ButtonGlobal;
--- src/pages/towBar.js
+++ src/pages/towBar.js
@@ -5,17 +5,23 @@ import Layout from '../components/Layout';
 import PageHeading from '../components/PageHeading';
 import TowBarHero from '../components/TowBarHero';
 
-export default class TowBarPage extends React.Component {
-  render() {
-    return (
-      <Layout>
-        <Seo title="Home" description="Welcome to GatsbyJs v1" />
-        <PageHeading
-          title="The Tow Bar"
-          subTitle="Our converted trailer bar makes a great addition to any event. The To Bar is ideal for serving welcome drinks as your guests arrive."
-        />
-        <TowBarHero towBgImage="/images/towbar/photo@2x.png" />
-      </Layout>
-    );
-  }
-}
+const TowBarPage = () => (
+  <Layout>
+    <Seo title="Home" description="Welcome to GatsbyJs v1" />
+    <PageHeading
+      title="The Tow Bar"
+      subTitle="Our converted trailer bar makes a great addition to any event. The To Bar is ideal for serving welcome drinks as your guests arrive."
+    />
+    <TowBarHero towBgImage="/images/towbar/photo@2x.png" />
+    <div className="columns">
+      <div className="column">
+        <img src="/images/towbar/hire.png" alt="" />
+      </div>
+      <div className="column">
+        <img src="/images/towbar/hire-2.png" alt="" />
+      </div>
+    </div>
+  </Layout>
+);
+
+export default TowBarPage;
