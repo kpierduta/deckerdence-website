2bfd9c9b817dfa7ecf69dc2e3bf4d91a614315d9
gagansaini1212 <gagansaini1212@gmail.com>
2020-02-14 17:11:44 +0530

    completed gallery hospitality page

---
+++ src/pages/galleryHospitality.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import Seo from '../components/Seo';
+import Layout from '../components/Layout';
+import PageHeading from '../components/PageHeading';
+import PageTitle from '../components/elements/PageTitle';
+import BlackButton from '../components/elements/BlackButton';
+import Testimonials from '../components/Testimonials';
+
+const GalleryHospitality = () => (
+  <Layout>
+    <Seo title="Home" />
+    <PageHeading title="GALLERY" />
+    <PageTitle title="Hospitality" />
+    <div className="columns is-multiline">
+      <Testimonials
+        bgImage="/images/gallery/photo-1@2x.png"
+        title="Lanterns and chandeliers"
+      />
+      <Testimonials
+        bgImage="/images/gallery/photo-1@2x.png"
+        title="colourful decor"
+      />
+      <Testimonials
+        bgImage="/images/gallery/photo-1@2x.png"
+        title="unique party Venue"
+      />
+      <Testimonials
+        bgImage="/images/gallery/photo-1@2x.png"
+        title="Maz’s bespoke party setup"
+      />
+      <Testimonials
+        bgImage="/images/gallery/photo-1@2x.png"
+        title="party styling"
+      />
+      <Testimonials
+        bgImage="/images/gallery/photo-1@2x.png"
+        title="cocktails on the upper deck"
+      />
+      <BlackButton />
+    </div>
+  </Layout>
+);
+
+export default GalleryHospitality;
