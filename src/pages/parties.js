d0adb37ca52f6733ef366aea290528f7bb31d3b8
dharmveer97 <dvbangar8@gmail.com>
2020-02-17 13:46:02 +0530

    add gallery in parties page.

--- src/pages/parties.js
+++ src/pages/parties.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Seo from '../components/Seo';
 import Layout from '../components/Layout';
@@ -8,7 +9,12 @@ import StagesSection from '../components/StagesSection';
 import VenueSection from '../components/VenueSection';
 import DetailsCardSection from '../components/DetailsCardSection';
 import GalleryItem from '../components/GalleryItem';
+import BlackButton from '../components/elements/BlackButton';
 import ContactForm from '../components/ContactForm';
+
+const GalleryWrapper = styled.div`
+  margin-top: 4rem;
+`;
 
 const Parties = () => (
   <Layout>
@@ -37,7 +43,49 @@ const Parties = () => (
       alt="main"
       frameBgImage="/images/parties/photo-black-and-white@2x.png"
     />
-    <GalleryItem />
+    <GalleryWrapper className="columns is-variable is-3 is-multiline">
+      <GalleryItem
+        imageColoured="/images/events/one-black.png"
+        imageBw="/images/events/one-colored.png"
+        altColour="colored image one"
+        altBw="black image one"
+      />
+      <GalleryItem
+        imageColoured="/images/events/second-colored.png"
+        imageBw="/images/events/second-black.png"
+        altColour="colored image second"
+        altBw="black image second"
+      />
+      <GalleryItem
+        imageColoured="/images/events/third-black.png"
+        imageBw="/images/events/third-colored.png"
+        altColour="colored image third"
+        altBw="black image third"
+      />
+      <GalleryItem
+        imageColoured="/images/events/fourth-black.png"
+        imageBw="/images/events/fourth-colored.png"
+        altColour="colored image fourth"
+        altBw="black image fourth"
+      />
+      <GalleryItem
+        imageColoured="/images/events/fifth-black.png"
+        imageBw="/images/events/fifth-colored.png"
+        altColour="colored image fifth"
+        altBw="black image fifth"
+      />
+      <GalleryItem
+        imageColoured="/images/events/sixth-black.png"
+        imageBw="/images/events/sixth-colored.png"
+        altColour="colored image sixth"
+        altBw="black image sixth"
+      />
+    </GalleryWrapper>
+    <BlackButton
+      image="/images/parties/view-more@2x.png"
+      alt="Learn More Button"
+      hasWidth="20%"
+    />
     <ContactForm />
   </Layout>
 );
