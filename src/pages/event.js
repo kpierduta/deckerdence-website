199e3a8cc789733601bae3b0e90822f73a7dd618
dharmveer97 <dvbangar8@gmail.com>
2020-02-17 21:07:21 +0530

    event page gallery fixed.

--- src/pages/event.js
+++ src/pages/event.js
@@ -9,14 +9,13 @@ import StagesSection from '../components/StagesSection';
 import Festival from '../components/Festival';
 import DetailsCardSection from '../components/DetailsCardSection';
 import HomePageSection from '../components/HomePageSection';
-import Button from '../components/elements/Button';
+import GalleryItem from '../components/GalleryItem';
+import BlackButton from '../components/elements/BlackButton';
 
-const Section = styled.div`
-  margin-top: 5rem;
-  .button-styled {
-    margin: 2rem 0;
-  }
+const GalleryWrapper = styled.div`
+  margin-top: 4rem;
 `;
+
 const Event = () => (
   <Layout>
     <Seo title="Event" description="Event Organized by Deckerdence" />
@@ -48,35 +47,50 @@ const Event = () => (
       bgImg="/images/events/middle-bg.png"
       bannerImg="/images/events/frame-1.png"
     />
-    <Section>
-      <div className="columns is-multiline">
-        <div className="column is-4">
-          <img src="/images/events/middle-bg.png" alt="" />
-        </div>
-        <div className="column is-4">
-          <img src="/images/events/middle-bg.png" alt="" />
-        </div>
-        <div className="column is-4">
-          <img src="/images/events/middle-bg.png" alt="" />
-        </div>
-        <div className="column is-4">
-          <img src="/images/events/middle-bg.png" alt="" />
-        </div>
-        <div className="column is-4">
-          <img src="/images/events/middle-bg.png" alt="" />
-        </div>
-        <div className="column is-4">
-          <img src="/images/events/middle-bg.png" alt="" />
-        </div>
-      </div>
-      <div className="has-text-centered button-styled">
-        <Button
-          image="/images/view-more-black.png"
-          alt="Learn More Button"
-          hasWidth="20%"
-        />
-      </div>
-    </Section>
+    <GalleryWrapper className="columns is-variable is-3 is-multiline">
+      <GalleryItem
+        imageColoured="/images/events/one-black.png"
+        imageBw="/images/events/one-colored.png"
+        altColour="colored image one"
+        altBw="black image one"
+      />
+      <GalleryItem
+        imageColoured="/images/events/second-colored.png"
+        imageBw="/images/events/second-black.png"
+        altColour="colored image second"
+        altBw="black image second"
+      />
+      <GalleryItem
+        imageColoured="/images/events/third-black.png"
+        imageBw="/images/events/third-colored.png"
+        altColour="colored image third"
+        altBw="black image third"
+      />
+      <GalleryItem
+        imageColoured="/images/events/fourth-black.png"
+        imageBw="/images/events/fourth-colored.png"
+        altColour="colored image fourth"
+        altBw="black image fourth"
+      />
+      <GalleryItem
+        imageColoured="/images/events/fifth-black.png"
+        imageBw="/images/events/fifth-colored.png"
+        altColour="colored image fifth"
+        altBw="black image fifth"
+      />
+      <GalleryItem
+        imageColoured="/images/events/sixth-black.png"
+        imageBw="/images/events/sixth-colored.png"
+        altColour="colored image sixth"
+        altBw="black image sixth"
+      />
+    </GalleryWrapper>
+
+    <BlackButton
+      image="/images/view-more-black.png"
+      alt="Learn More Button"
+      hasWidth="20%"
+    />
   </Layout>
 );
 
