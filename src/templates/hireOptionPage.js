9922adf5c9714f7fbd2a8feb76723a8850ef180c
dharmveer97 <dvbangar8@gmail.com>
2020-02-28 15:02:50 +0530

    minor fixes.

--- src/templates/hireOptionPage.js
+++ src/templates/hireOptionPage.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import { graphql } from 'gatsby';
-import styled from 'styled-components';
 
 import Seo from '../components/Seo';
 import Layout from '../components/Layout';
-import PartiesHero from '../components/PartiesHero';
-import PageHeading from '../components/PageHeading';
-import StagesSection from '../components/StagesSection';
-import Festival from '../components/Festival';
-import DetailsCardSection from '../components/DetailsCardSection';
-import HomePageSection from '../components/HomePageSection';
-import GalleryItem from '../components/GalleryItem';
-import BlackButton from '../components/elements/BlackButton';
-
-const GalleryWrapper = styled.div`
-  margin-top: 4rem;
-`;
+import HireHero from '../components/HireHero';
+import HireForm from '../components/HireForm';
+import TileItem from '../components/TileItem';
+import BlogCategoryItem from '../components/BlogCategoryItem';
+import HireCardSlider from '../components/HireCardSlider';
+import Diagram from '../components/Diagram';
+import VenueSection from '../components/VenueSection';
+import ContactForm from '../components/ContactForm';
 
 export const hireOptionPage = graphql`
   query hireOption($slug: String) {
@@ -32,80 +27,77 @@ const HireOptionPage = ({ data }) => {
   const hire = data.sanityVehicleHirePage;
   return (
     <Layout>
-      <Seo title="Event" description="Event Organized by Deckerdence" />
-      <PartiesHero
-        title={hire.hireOptionTitle}
-        partyBgImage="/images/parties/photo@2x.png"
-        ball="/images/parties/disco-ball-copy@2x.png"
+      <Seo title="Deckerdence Hire Page" />
+      <HireHero
+        Option={hire.hireOptionTitle}
+        title="Deckerdence Vintage Bus Bar"
+        description="BESPOKE DRINKS OPTIONS AVAILABLE"
       />
-      <PageHeading title="We can provide everything you need for the ultimate event" />
-      <StagesSection />
-      <HomePageSection
+      <section className="section">
+        <div className="container">
+          <div className="columns is-multiline">
+            <BlogCategoryItem category="Vintage Bus Bar" sideImage />
+            <BlogCategoryItem category="Fully Customisable" sideImage />
+            <BlogCategoryItem category="Cocktail Lounge" sideImage />
+            <BlogCategoryItem category="Half Wraparound Marquee" />
+          </div>
+        </div>
+      </section>
+      <HireForm
+        para="Deckerdence Bus Bar with the 1/2 wrap-around marquee is a great option for entertaining. Perfect for pitchside hospitality, or a racecourse or polo match. Our team will come along and carry out a site visit to your location, and then we will work with you to ensure you have everything perfect for your event. We have a range of items that furnish Deckerdence perfectly."
+        avatar="/images/hire/icon@2x.png"
+        email="Email"
+        phone="Phone Number"
+        buttonName="DOWNLOAD"
+        subtitle="By submitting this form you agree to our contact conditions."
+        mainSubtitle="mainSubtitle"
+      />
+      <HireCardSlider />
+      <section className="section">
+        <div className="container">
+          <div className="columns is-multiline is-variable is-4">
+            <TileItem
+              title="Marquee information"
+              subtitle="The half wrap around marquee measures 25m x 9m and the marquee structure is fitted directly from the bus, between the
+            2 decks, to not spoil the upstairs views."
+            />
+            <TileItem
+              title="Vintage Lounge"
+              subtitle="Our vintage lounge consists of a 3 seat sofa,a 2 seat sofa, Chesterfield armchair and wingback chair positioned around a fire place with an electric working fire"
+            />
+            <TileItem
+              title="Hire includes"
+              subtitle="Your hire includes: marquee flooring,chandelier lighting, an external generator that provides the power for your event (if required) and fuel for 12 hours running time."
+            />
+            <TileItem
+              title="Compliment your hire"
+              subtitle="We have a range of options that you can add to compliment your hire. One such item is our chequered black and white dance floor measures 5m x 5m and is lit up by our disco and effect lighting"
+            />
+          </div>
+        </div>
+      </section>
+      <Diagram avatar="/images/hire/bus-diagram.png" />
+      <VenueSection
+        color
+        frame="/images/parties/frame@2x.png"
+        text="Cocktail Lounge Upstairs in Vintage bus bar "
+        alt="main"
+        frameBgImage="/images/hire/hire-bg-one.png"
+      />
+      <VenueSection
         flex
-        title="Deckerdence is a unique venue that will make your event stand out from the crowd! "
-        bgImg="/images/events/middle-bg.png"
-        bannerImg="/images/events/frame-1.png"
+        frame="/images/parties/frame@2x.png"
+        text="Plan every Detail with expert support"
+        alt="main"
+        frameBgImage="/images/hire/hire-bg-second.png"
       />
-      <Festival
-        title="OUR VENUE IS PERFECT FOR..."
-        subtitle="...AND CAN TRAVEL ANYWHERE IN THE UK"
+      <VenueSection
+        color
+        frame="/images/parties/rightframes.png"
+        text="Create Your Theme Fully Customisable"
+        frameBgImage="/images/parties/photo-black-and-white@2x.png"
       />
-      <HomePageSection
-        title="Deckerdence is a unique venue that will make your event stand out from the crowd! "
-        bgImg="/images/events/bottom-bg-2.png"
-        bannerImg="/images/events/banner-light.png"
-      />
-      <DetailsCardSection />
-      <HomePageSection
-        flex
-        title="Deckerdence is a unique venue that will make your event stand out from the crowd! "
-        bgImg="/images/events/middle-bg.png"
-        bannerImg="/images/events/frame-1.png"
-      />
-      <GalleryWrapper className="columns is-variable is-3 is-multiline">
-        <GalleryItem
-          imageColoured="/images/events/one-black.png"
-          imageBw="/images/events/one-colored.png"
-          altColour="colored image one"
-          altBw="black image one"
-        />
-        <GalleryItem
-          imageColoured="/images/events/second-colored.png"
-          imageBw="/images/events/second-black.png"
-          altColour="colored image second"
-          altBw="black image second"
-        />
-        <GalleryItem
-          imageColoured="/images/events/third-black.png"
-          imageBw="/images/events/third-colored.png"
-          altColour="colored image third"
-          altBw="black image third"
-        />
-        <GalleryItem
-          imageColoured="/images/events/fourth-black.png"
-          imageBw="/images/events/fourth-colored.png"
-          altColour="colored image fourth"
-          altBw="black image fourth"
-        />
-        <GalleryItem
-          imageColoured="/images/events/fifth-black.png"
-          imageBw="/images/events/fifth-colored.png"
-          altColour="colored image fifth"
-          altBw="black image fifth"
-        />
-        <GalleryItem
-          imageColoured="/images/events/sixth-black.png"
-          imageBw="/images/events/sixth-colored.png"
-          altColour="colored image sixth"
-          altBw="black image sixth"
-        />
-      </GalleryWrapper>
-
-      <BlackButton
-        image="/images/view-more-black.png"
-        alt="Learn More Button"
-        hasWidth="20%"
-      />
+      <ContactForm />
     </Layout>
   );
 };
