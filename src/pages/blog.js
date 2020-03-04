0d050e7a347d25d9464f651bca8d9ab6571f7426
dharmveer97 <dvbangar8@gmail.com>
2020-03-05 00:32:43 +0530

    add  blogs on sanity.

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,16 +1,70 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Seo from '../components/Seo';
 import Layout from '../components/Layout';
 import PageHeading from '../components/PageHeading';
-import BlogTile from '../components/BlogTile';
+import BlogData from '../components/BlogData';
 
-const BlogPage = () => (
-  <Layout>
-    <Seo title="Blog" description="Deckerdence Blog" />
-    <PageHeading title="BLOG" />
-    <BlogTile />
-  </Layout>
-);
+const Section = styled.section`
+  .column {
+    display: flex;
+  }
+`;
+
+export const blogQuery = graphql`
+  query blog {
+    sanitySiteSettings {
+      blogSeoTitle
+      blogSeoKeywords
+      blogSeoMetaDescription
+      blogBarMainTitle
+      blogs {
+        items {
+          isSizeHalf
+          title
+          subtitle
+          image {
+            asset {
+              url
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+const BlogPage = ({ data }) => {
+  const page = data.sanitySiteSettings;
+  return (
+    <Layout>
+      <Seo
+        title="Blog"
+        description="Deckerdence Blog"
+        keywords="Deckerdence Blog"
+      />
+      <PageHeading title="BLOG" />
+      <Section className="section">
+        <div className="columns is-multiline">
+          {page.blogs.items.map(items => (
+            <div
+              className={
+                items.isSizeHalf ? 'column is-6 is-flex' : 'column is-3 is-flex'
+              }
+            >
+              <BlogData
+                color
+                title={items.title}
+                subtitle={items.subtitle}
+                boxImage={items.image.asset.url}
+              />
+            </div>
+          ))}
+        </div>
+      </Section>
+    </Layout>
+  );
+};
 
 export default BlogPage;
