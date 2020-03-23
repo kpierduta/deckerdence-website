5326234ceb25305975b7003a8155a00a40de3c79
dharmveer97 <dvbangar8@gmail.com>
2020-03-23 18:22:06 +0530

    add Responsive Image with fixed height

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -3,58 +3,48 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Section = styled.div`
-  .hoverBox {
+  .main {
     position: relative;
-    display: inline-block;
-    overflow: hidden;
-    max-width: 100%;
-    height: auto;
   }
-  .hoverBox img {
-    max-width: 100%;
-  }
-  .hoverBox .hoverBox-layer_bottom {
+  .image {
     display: block;
   }
-  .hoverBox .hoverBox-layer_top {
-    opacity: 0;
+  .overlay {
     position: absolute;
     top: 0;
+    bottom: 0;
     left: 0;
     right: 0;
-    bottom: 0;
-    width: 100%;
     height: 100%;
-    background: rgba(0, 0, 0, 0.6);
-    color: #fff;
-    padding: 15px;
-    transition: all 0.4s ease-in-out 0s;
+    width: 100%;
+    opacity: 0;
+    transition: 0.5s ease;
+    background-color: #008cba;
   }
-  .hoverBox:hover .hoverBox-layer_top {
+  .main:hover .overlay {
     opacity: 1;
   }
-  .hoverBox .hoverBox-text {
-    text-align: center;
-    display: inline-block;
+  .text {
+    color: white;
+    font-size: 20px;
     position: absolute;
     top: 50%;
     left: 50%;
-    -moz-transform: translate(-50%, -50%);
     -webkit-transform: translate(-50%, -50%);
     -ms-transform: translate(-50%, -50%);
     transform: translate(-50%, -50%);
-  }
-  .hoverBox.active .hoverBox-text {
-    display: block;
+    text-align: center;
   }
 `;
 
 const Testimonials = ({ Image, title }) => (
   <Section className="column is-4">
-    <div className="hoverBox">
-      <img src={Image} alt="images" className="hoverBox-layer_bottom" />
-      <div className="hoverBox-layer_top">
-        <div className="hoverBox-text">
+    <div className="main">
+      <figure className="image is-square">
+        <img src={Image} alt="Avatar" className="image" />
+      </figure>
+      <div className="overlay">
+        <div className="text">
           <h1 className="title is-2 has-text-white has-text-centered is-size-4-touch">
             {title}
           </h1>
