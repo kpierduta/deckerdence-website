3fec9ff7a8c997f1b66863fd48107c523886b604
dharmveer97 <dvbangar8@gmail.com>
2020-04-14 16:34:22 +0530

    monor fixes

--- src/components/Diagram.js
+++ src/components/Diagram.js
@@ -8,7 +8,7 @@ const Diagram = ({ avatar, largeImage }) => (
   <Section className="section">
     <div className="container">
       <div className="columns is-centered">
-        <div className="column is-7">
+        <div className="column is-6">
           <figure className={largeImage ? 'image is-1by1' : 'image is-3by2'}>
             <img src={avatar} alt="" />
           </figure>
