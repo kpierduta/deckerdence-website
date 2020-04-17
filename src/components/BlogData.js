8adb6692f67709937b742b53559038c7872dd81f
dharmveer97 <dvbangar8@gmail.com>
2020-04-17 21:55:05 +0530

    Fix Blogs.

--- src/components/BlogData.js
+++ src/components/BlogData.js
@@ -3,7 +3,7 @@ import { Link } from 'gatsby';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-  min-height:14rem;
+  min-height:20rem;
   background-image: url('${props => props.boxImage}');
   background-size: 100% 100%;
   background-repeat: no-repeat;
@@ -21,21 +21,22 @@ const Wrapper = styled.div`
   }
   .has-text-centered {
     align-self: end;
+    margin: 0 0 10px;
   }
 `;
 
 const BlogData = ({ title, date, boxImage, color, link }) => (
   <Wrapper className="tile is-child box" boxImage={boxImage} color={color}>
-    <p className="title is-5 has-text-weight-semibold has-text-centered text">
+    <p className="title is-4 has-text-weight-semibold has-text-centered text">
       {title}
     </p>
-    <p className="subtitle is-5 has-text-centered has-text-weight-semibold text">
+    <p className="subtitle is-4 has-text-centered has-text-weight-semibold text">
       {date}
     </p>
     <div className="has-text-centered">
       <Link
         to={link}
-        className="button is-normal title is-5 has-text-weight-normal is-family-secondary text"
+        className="button is-normal title is-4 has-text-weight-normal is-family-secondary text"
         type="button"
       >
         READ MORE
