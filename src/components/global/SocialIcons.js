b6ea749f9dacf9e81d3aed9cef4daff56fb75815
gagansaini1212 <gagansaini1212@gmail.com>
2022-02-08 11:56:41 +0530

    added social icon component

--- src/components/global/SocialIcons.js
+++ src/components/global/SocialIcons.js
@@ -1,21 +1,22 @@
 import React from 'react';
-import { useStaticQuery, graphql } from 'gatsby';
+// import { useStaticQuery, graphql } from 'gatsby';
 import styled from 'styled-components';
 
-import { FaYoutube } from 'react-icons/fa';
-import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
-import { BsFacebook } from 'react-icons/bs';
+// import { FaYoutube } from 'react-icons/fa';
+// import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
+// import { BsFacebook } from 'react-icons/bs';
+import config from '../../utils/config';
 
-const query = graphql`
-  query socialUrlQuery {
-    sanityGlobals {
-      instagram
-      facebook
-      youtube
-      twitter
-    }
-  }
-`;
+// const query = graphql`
+//   query socialUrlQuery {
+//     sanityGlobals {
+//       instagram
+//       facebook
+//       youtube
+//       twitter
+//     }
+//   }
+// `;
 
 const Container = styled.div``;
 // const Container = styled.div(({ flex }) => [
@@ -37,55 +38,59 @@ const IconWrapper = styled.div``;
 //   variant === 'secondary' && tw`my-2`,
 // ]);
 
-const socialNetworks = ({ twitter, facebook, instagram, youtube }) => {
-  const items = [];
-  if (twitter) {
-    items.push({
-      id: 1,
-      name: 'twitter',
-      icon: <AiOutlineTwitter />,
-      link: twitter,
-    });
-  }
-  if (facebook) {
-    items.push({
-      id: 2,
-      name: 'facebook',
-      icon: <BsFacebook />,
-      link: facebook,
-    });
-  }
-  if (instagram) {
-    items.push({
-      id: 3,
-      name: 'instagram',
-      icon: <AiFillInstagram />,
-      link: instagram,
-    });
-  }
-  if (youtube) {
-    items.push({
-      id: 4,
-      name: 'youtube',
-      icon: <FaYoutube />,
-      link: youtube,
-    });
-  }
-  return items;
-};
+// const socialNetworks = ({ twitter, facebook, instagram, youtube }) => {
+//   const items = [];
+//   if (twitter) {
+//     items.push({
+//       id: 1,
+//       name: 'twitter',
+//       icon: <AiOutlineTwitter />,
+//       link: twitter,
+//     });
+//   }
+//   if (facebook) {
+//     items.push({
+//       id: 2,
+//       name: 'facebook',
+//       icon: <BsFacebook />,
+//       link: facebook,
+//     });
+//   }
+//   if (instagram) {
+//     items.push({
+//       id: 3,
+//       name: 'instagram',
+//       icon: <AiFillInstagram />,
+//       link: instagram,
+//     });
+//   }
+//   if (youtube) {
+//     items.push({
+//       id: 4,
+//       name: 'youtube',
+//       icon: <FaYoutube />,
+//       link: youtube,
+//     });
+//   }
+//   return items;
+// };
+
+const socialIcons = [
+  { key: '1', icon: '/images/icon/facebook.png', link: config.facebook },
+  { key: '2', icon: '/images/icon/twitter.png', link: config.twitter },
+  { key: '3', icon: '/images/icon/linked-in.png', link: config.linkedin },
+  { key: '4', icon: '/images/icon/instagram.png', link: config.instagram },
+  { key: '5', icon: '/images/icon/pinterest.png', link: config.pinterest },
+  { key: '6', icon: '/images/icon/youtube.png', link: config.youTube },
+];
 
 const SocialIcons = ({ variant, ...props }) => {
-  const { sanityGlobals: data } = useStaticQuery(query);
+  // const { sanityGlobals: data } = useStaticQuery(query);
 
   return (
     <Container {...props}>
-      {socialNetworks({
-        twitter: data.twitter,
-        facebook: data.facebook,
-        instagram: data.instagram,
-        youtube: data.youtube,
-      }).map(item => (
-        <IconWrapper key={item.id} type={item.name} variant={variant}>
+      {socialIcons.map(item => (
+        <IconWrapper key={item.id} type={item.icon}>
           <a
             href={item.link}
             tw="cursor-pointer text-xl relative text-white"
--- src/pages/test.js
+++ src/pages/test.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import SocialIcons from '../components/global/SocialIcons';
-
 import Layout from '../components/Layout';
 
 const Test = () => (
   <Layout>
     <p>This is test page</p>
-    <SocialIcons />
   </Layout>
 );
 
