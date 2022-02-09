d93cff8c748d20e8d331dcccf06a35ad6d9afc57
gagansaini1212 <gagansaini1212@gmail.com>
2022-02-09 14:06:56 +0530

    added title component

---
+++ src/components/newComponents1/Title.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Title = ({ title }) => (
+  <div>
+    <span className="title is-uppercase is-size-3-mobile">{title}</span>
+  </div>
+);
+
+export default Title;
