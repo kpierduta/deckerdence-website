47e8c870fd0a26dc6c55c688e554bbd5c5132ec7
dharmveer97 <dvbangar8@gmail.com>
2022-02-12 20:02:27 +0530

    fixed

--- schemas/blocks/galleryBlock.js
+++ schemas/blocks/galleryBlock.js
@@ -19,11 +19,13 @@ export default {
     select: {
       title: 'galleryBlock.0.title',
       image: 'galleryBlock.0.image',
+      subtitle: 'subtitle',
     },
     prepare({ title, image }) {
       return {
         title: title || '🍀 Gallery Block',
         media: image,
+        subtitle: 'Three images gallery',
       };
     },
   },
