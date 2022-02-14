8915923e10d5f88ca3ee67afa4baf4c66c31351f
gagansaini1212 <gagansaini1212@gmail.com>
2022-02-14 13:00:02 +0530

    fixed #28

--- src/components/hero/HomeBanner.js
+++ src/components/hero/HomeBanner.js
@@ -2,7 +2,7 @@ import React from 'react';
 import SanityImage from '../global/SanityImage';
 
 const HeroBanner = ({ image }) => {
-  return <SanityImage fluid={image.asset.id} />;
+  return <SanityImage fluid={image.asset.id} width="2000" />;
 };
 
 export default HeroBanner;
