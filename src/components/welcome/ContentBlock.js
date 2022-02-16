e07177e29b566e8dbb24623afd872f2426bc2ed2
dharmveer97 <dvbangar8@gmail.com>
2022-02-16 16:27:28 +0530

    fixed

--- src/components/welcome/ContentBlock.js
+++ src/components/welcome/ContentBlock.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 import { PortableText } from '../elements';
 
-const ContentBlock = ({ description, isDark }) => (
+const ContentBlock = ({ description, isDark, targetButton }) => (
   <section className="section">
     <div className="container has-text-centered">
       <div
@@ -13,6 +14,13 @@ const ContentBlock = ({ description, isDark }) => (
           <div className="has-text-white mt-4">
             <PortableText light={!isDark}>{description}</PortableText>
           </div>
+        )}
+        {targetButton && (
+          <Link
+            className="button is-danger mt-5 has-text-weight-bold"
+            to={targetButton || '/'}>
+            Find Out More
+          </Link>
         )}
       </div>
     </div>
